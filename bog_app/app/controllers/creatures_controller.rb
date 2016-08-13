3a14471ea839b060ddec0e57140f97375ddb5965
jsonlitz <jasondotlee@gmail.com>
2016-08-13 15:39:08 -0700

    added update and edit features

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -27,12 +27,36 @@ class CreaturesController < ApplicationController
   def show
     # get the creature id from the url params
     creature_id = params[:id]
-
     # use `creature_id` to find the creature in the database
     # and save it to an instance variable
     @creature = Creature.find_by_id(creature_id)
-
     # render the show view (it has access to instance variable)
     render :show
   end
-end
+
+  def edit
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+    # render the edit view (it has access to instance variable)
+    render :edit
+  end
+
+  def update
+       # get the creature id from the url params
+       creature_id = params[:id]
+       # use `creature_id` to find the creature in the database
+       # and save it to an instance variable
+       creature = Creature.find_by_id(creature_id)
+       # whitelist params and save them to a variable
+       creature_params = params.require(:creature).permit(:name, :description)
+       # update the creature
+       creature.update_attributes(creature_params)
+       # redirect to show page for the updated creature
+       redirect_to creature_path(creature)
+       # redirect_to creature_path(creature) is equivalent to:
+       # redirect_to "/creatures/#{creature.id}"
+     end
+   end
