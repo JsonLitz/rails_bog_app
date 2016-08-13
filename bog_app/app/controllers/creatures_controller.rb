332a5f0563f2c1bca1d344081d30182d7ad5b16b
jsonlitz <jasondotlee@gmail.com>
2016-08-13 15:43:22 -0700

    added delete REST and controller

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -58,5 +58,23 @@ class CreaturesController < ApplicationController
        redirect_to creature_path(creature)
        # redirect_to creature_path(creature) is equivalent to:
        # redirect_to "/creatures/#{creature.id}"
-     end
-   end
+  end
+
+  def destroy
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+
+    # destroy the creature
+    creature.destroy
+
+    # redirect to creatures index
+    redirect_to creatures_path
+    # redirect_to creatures_path is equivalent to:
+    # redirect_to "/creatures"
+  end
+
+end
