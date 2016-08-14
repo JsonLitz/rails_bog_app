6519bb8dd5f07e125f538978d49850bf5524f41c
jsonlitz <jasondotlee@gmail.com>
2016-08-14 15:09:55 -0700

    added upated route and defined update method in controller

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -39,4 +39,18 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
     render :edit
   end
+
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+    # update the creature
+    creature.update_attributes(creature_params)
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+    # redirect_to "/creatures/#{creature.id}"
+  end
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   post "/creatures", to: "creatures#create"
   get "/creatures/:id", to: "creatures#show", as: "creature"
   get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
-  put "/creatures/:id/update", to: "creatures#update"
+  patch "/creatures/:id", to: "creatures#update"
  end
