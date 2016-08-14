9c04b444faa25855290998838f324e9649ee14fa
jsonlitz <jasondotlee@gmail.com>
2016-08-14 15:17:00 -0700

    added delete route, method, and button

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -53,4 +53,11 @@ class CreaturesController < ApplicationController
     # redirect_to creature_path(creature) is equivalent to:
     # redirect_to "/creatures/#{creature.id}"
   end
+
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
 end
