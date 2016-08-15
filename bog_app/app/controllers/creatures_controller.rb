d90cddebd39bf770fe63a3de510ff676c3063d6d
jsonlitz <jasondotlee@gmail.com>
2016-08-14 22:02:48 -0700

    added patch and delete restful

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -26,7 +26,23 @@ class CreaturesController < ApplicationController
   def edit
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
-    render :show    
+    render :edit
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+
   end
 
 end
