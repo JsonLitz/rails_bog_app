ed2488a4db7aab7c0bc65cad9239d9d235ff82a6
jsonlitz <jasondotlee@gmail.com>
2016-08-14 20:34:30 -0700

    added restful create route, controller and view

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "creatures#index"
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/:id", to: "creatures#new"
+  post "/creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
 end
